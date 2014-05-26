bae810bf72bb0001ea5fee40c57ce5388aa15a50
Lawrence Ting <lawrencessting@gmail.com>
2014-05-26 17:42:29 +0930

    .

--- public/js/social.js
+++ public/js/social.js
@@ -1,22 +1,14 @@
+var scripts = {
+    'facebook-jssdk': '//connect.facebook.net/en_US/all.js#xfbml=1',
+    'googleplus'    : 'https://apis.google.com/js/plusone.js',
+    'twitter-wjs'   : '//platform.twitter.com/widgets.js',
+    'analytics'     : ('https:'==location.protocol?'//ssl':'//www') + '.google-analytics.com/ga.js'
+  }, script, _gaq=[['_setAccount','UA-47200464-1'],['_trackPageview']];
 
-
-    $('#demo1').sharrre({
-    share: {
-    googlePlus: true,
-    facebook: true,
-    twitter: true
-    },
-    buttons: {
-    googlePlus: {size: 'tall', annotation:'bubble'},
-    facebook: {layout: 'box_count'},
-    twitter: {count: 'vertical', via: '_JulienH'}
-    },
-    hover: function(api, options){
-    $(api.element).find('.buttons').show();
-    },
-    hide: function(api, options){
-    $(api.element).find('.buttons').hide();
-    },
-    enableTracking: true
-    });
-
+  for (var id in scripts) {
+    script = document.createElement('script');
+    script.src = scripts[id]; script.id = id;
+    script.type = 'text/javascript';
+    script.async = true;
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+  }
