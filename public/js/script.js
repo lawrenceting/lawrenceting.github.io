1d884a86f51054d0c58a7c6e77d8bbec75fc1a05
Lawrence Ting <lawrencessting@gmail.com>
2014-08-06 01:48:09 +0930

    .

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	
+	sortMenu(".sidebar-nav", "li"); //Sort menu alphabetically
+
+});
+
+function sortMenu(selector, theChild) {
+    $(selector).children(theChild).sort(function(a, b) {
+        var upA = $(a).text().toUpperCase();
+        var upB = $(b).text().toUpperCase();
+        return (upA < upB) ? -1 : (upA > upB) ? 1 : 0;
+    }).appendTo(selector);
+}
